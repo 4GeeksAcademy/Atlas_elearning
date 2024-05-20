65bcd8a760fa80891597a6d81ec0f07d3d163ec0
Alejandro J Cisnero A <114025066+alecisnero@users.noreply.github.com>
2024-05-20 02:49:08 +0000

    fix/update

---
+++ migrations/versions/f07ab2e624ba_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: f07ab2e624ba
+Revises: d61fc7c8adb7, 208a0f5b30f1
+Create Date: 2024-05-20 02:25:54.731884
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f07ab2e624ba'
+down_revision = ('d61fc7c8adb7', '208a0f5b30f1')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
